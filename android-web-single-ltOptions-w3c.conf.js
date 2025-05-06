44769c4629a3d7b219e5e9351daf42d9f3a739ee
Ankitac3003 <119027040+Ankitac3003@users.noreply.github.com>
2025-05-06 22:49:36 +0530

    Update android-web-single-ltOptions-w3c.conf.js

--- android-web-single-ltOptions-w3c.conf.js
+++ android-web-single-ltOptions-w3c.conf.js
@@ -20,7 +20,7 @@ exports.config = {
           isRealMobile: true,
           enableCustomTranslation: true,
           w3c: true,
-          build: process.env.LT_BUILD_NAME
+          build: "Sample Build"
         }
       },
     ],
